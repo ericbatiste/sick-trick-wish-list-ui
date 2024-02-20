fd675b7a0a0093614bce940b4afce0265ef7ff54
Eric Batiste <ericbatiste4@gmail.com>
2024-02-20 10:58:39 -0700

    Build out Form, add functionality to render new tricks to App component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import Form from '../Form/Form';
 import Tricks from '../Tricks/Tricks';
 
 
 
 function App() {
   const [tricks, setTricks] = useState([]);
+
+  const addTrick = (newTrick) => {
+    setTricks([...tricks, newTrick])
+  }
 
   useEffect(() => {
     fetch('http://localhost:3001/api/v1/tricks')
@@ -16,6 +21,7 @@ function App() {
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
+      <Form addTrick={addTrick}/>
       <Tricks tricks={tricks} />
     </div>
   );
---
+++ src/components/Form/Form.js
@@ -0,0 +1,78 @@
+import './Form.css';
+import { useState } from 'react';
+
+function Form({addTrick}) {
+  const [stance, setStance] = useState('');
+  const [name, setName] = useState('');
+  const [obstacle, setObstacle] = useState('');
+  const [tutorial, setTutorail] = useState('');
+
+  const submitTrick = e => {
+    e.preventDefault();
+    const newTrick = {
+      stance,
+      name,
+      obstacle,
+      tutorial,
+      id: Date.now(),
+    }
+    addTrick(newTrick);
+    clearInputs()
+  }
+
+  const clearInputs = () => {
+    setStance('');
+    setName('');
+    setObstacle('');
+    setTutorail('');
+  }
+
+  return (
+    <form>
+      <select 
+        name="stance" 
+        id="stance-select"
+        value={stance}
+        onChange={e => setStance(e.target.value)}
+      >
+        <option value="">Choose your stance</option>
+        <option value="regular">regular</option>
+        <option value="goofy">goofy</option>
+      </select>
+      
+      <input 
+        type="text"
+        placeholder='Name of trick'
+        name='name'
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      
+      <select 
+        name="obstacle" 
+        id="obstacle-select"
+        value={obstacle} 
+        onChange={e => setObstacle(e.target.value)}
+      >
+        <option value="">Choose your obstacle</option>
+        <option value="flatground">flatground</option>
+        <option value="ledge">ledge</option>
+        <option value="rail">rail</option>
+        <option value="stairs">stairs</option>
+        <option value="pool">pool</option>
+      </select>
+      
+      <input 
+        type="text"
+        placeholder='Link to tutorial'
+        name='tutorialLink'
+        value={tutorial}
+        onChange={e => setTutorail(e.target.value)}
+      />
+      
+      <button onClick={e => submitTrick(e)}>SeNd iT!</button>
+    </form>
+  )
+}
+
+export default Form
