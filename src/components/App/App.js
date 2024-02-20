b80c5e3e2e70fc0bcbbb5777566d2731156a991a
Eric Batiste <ericbatiste4@gmail.com>
2024-02-20 10:14:34 -0700

    Implement get request when App component mounts.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,32 +5,12 @@ import Tricks from '../Tricks/Tricks';
 
 
 function App() {
-  const [tricks, setTricks] = useState([
-      {
-          "stance": "regular",
-          "name": "treflip",
-          "obstacle": "flat ground",
-          "tutorial": "https://www.youtube.com/watch?v=XGw3YkQmNig",
-          "id": 1
-      },
-      {
-          "stance": "switch",
-          "name": "heelflip",
-          "obstacle": "stairs",
-          "tutorial": "https://www.youtube.com/watch?v=9N9swrZU1HA",
-          "id": 2
-      },
-      {
-          "stance": "regular",
-          "name": "frontside 50-50, backside 180 out",
-          "obstacle": "ledge",
-          "tutorial": "https://www.youtube.com/watch?v=9N9swrZU1HA",
-          "id": 3
-      }
-  ]);
+  const [tricks, setTricks] = useState([]);
 
   useEffect(() => {
-
+    fetch('http://localhost:3001/api/v1/tricks')
+    .then(res => res.json())
+    .then(data => setTricks(data))
   }, [])
 
   return (
